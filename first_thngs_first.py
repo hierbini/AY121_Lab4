fb10f7a33ab38474856284fd553117b7364ead03
Christine Nguyen <nguyen.da.christine@gmail.com>
2019-04-14 13:01:54 -0700

    'fuck'

--- first_thngs_first.py
+++ first_thngs_first.py
@@ -1,15 +1,16 @@
 import tracking
 import leuschner
+import ugradio.leusch
 
 def take_observation(altitude, azimuth):
 	LT = ugradio.leusch.LeuschTelescope()
 	LT.point(altitude, azimuth)
 
-def save_observation(number_of_spectra, galactic_coordinates):
+def save_observation(filename, number_of_spectra, galactic_coordinates):
 	spectrometer = leuscher.Spectrometer('10.0.1.2')
 	spectrometer.read_spec("Data/" + filename, number_of_spectra, galactic_coordinates)
 
 l, b = 120, 0
 alt, az = tracking.get_altaz(l, b)
 take_observation(alt, az)
-save_observation(100, (l, b))
+save_observation("Test", 100, (l, b))
