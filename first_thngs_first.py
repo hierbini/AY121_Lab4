4032b4edb04fdf1c2303aa2c29be985790444a4a
Christine Nguyen <nguyen.da.christine@gmail.com>
2019-04-14 13:57:29 -0700

    'beep'

--- first_thngs_first.py
+++ first_thngs_first.py
@@ -5,12 +5,15 @@ import ugradio.leusch
 def take_observation(altitude, azimuth):
 	LT = ugradio.leusch.LeuschTelescope()
 	LT.point(altitude, azimuth)
+	
 
 def save_observation(filename, number_of_spectra, galactic_coordinates):
 	spectrometer = leuschner.Spectrometer('10.0.1.2')
 	spectrometer.read_spec(filename, number_of_spectra, galactic_coordinates)
 
-l, b = 120, 0
-alt, az = tracking.get_altaz(l, b)
-take_observation(alt, az)
-save_observation("Test.fits", 100, (l, b))
+
+def observation(filename)
+	l, b = 120, 0
+	alt, az = tracking.get_altaz(l, b)
+	take_observation(alt, az)
+	save_observation(filename + ".fits", 100, (l, b))
