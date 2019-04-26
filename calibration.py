0007833fa8099a15c44acdd114616dbd095fdd7c
Christine Nguyen <nguyen.da.christine@gmail.com>
2019-04-25 17:28:23 -0700

    finalized code

--- calibration.py
+++ calibration.py
@@ -1,25 +1,144 @@
-import numpy as np 
+import numpy as np
+import ugradio
+import scipy.signal 
+import astropy.io.fits as pyfits
+import tool_box as tb
 
-def mean_spec(spectra):
-	return np.mean(spectra, axis = 0)
 
-def son_soff(upperspec, lowerspec):
-    """
-    Creates online spectrum and offline spectrum from the upper spectra and lower spectra
-    
-    Parameters:
-    upperspec (list): upper spectrum
-    lowersepec (list): lower spectrum
-    
-    Returns:
-    s_on (array): online spectrum
-    s_off (array): offline spectrum
-    """
-    left_upper, right_upper = np.split(upperspec, 2)
-    left_lower, right_lower = np.split(lowerspec, 2)
-    s_off = list(right_upper) + list(left_lower)
-    s_on = list(left_upper) + list(right_lower)
-    return np.asarray(s_on), np.asarray(s_off)
+def get_spectra(observation, spectra_number, polarization="first"):
+	"""
+	Function retrieves a specified spectra from an observation file. 
 
-s_on, s_off = son_soff(mean_upperspec, mean_lowerspec)
-s_line = s_on/s_off
+	Parameters:
+	observation (file): file where spectra data is saved
+	spectra_number (int): index of specific spectra
+	polarization (string): polarization of telescope ('first' or 'second')
+
+	Returns:
+	spectra (numpy array)
+	"""
+    if polarization == "first":
+        pol = 'auto0_real'
+    elif polarization == "second":
+        pol = 'auto1_real'
+    return observation[spectra_number].data[pol]
+
+
+def average_spectra(observation, n_spectra):
+	"""
+	Gets the average spectra of all spectra taken during an observation
+
+	Parameters:
+	observation (file): file where spectra data is saved
+	n_spectra (int): the number of spectra taken during the observation
+
+	Returns:
+	average_spectra (numpy array)
+	"""
+    average_spectra = []
+    for i in np.arange(1, n_spectra):
+        average_spectra.append(get_spectra(observation, i))
+    return np.mean(np.array(average_spectra), axis = 0)
+
+
+def frequency(observation):
+	"""
+	Gets the frequency axis of the spectra in an observation
+
+	Parameters:
+	observation (file): file where spectra data is saved
+
+	Returns:
+	frequency (numpy array): a frequency array
+	"""
+	vsamp = observation[0].header["SAMPRATE"]
+	return tb.shift(tb.freq(len(average_spectra), d = 1/vsamp)) / 1e6 + 1420.4 + 150
+
+
+def baseline_fit(average_spectra, freq):
+	"""
+	Finds a fit to the baseline. Uses linear interpolation.
+
+	Parameters:
+	average_spectra (numpy array): the average spectra of all the spectra in an observation
+	freq (numpy array): the frequency array which corresponds to the spectra
+
+	Returns:
+	baseline (numpy array): the baseline fit
+	"""
+    filtered_spectra = scipy.signal.medfilt(average_spectra, kernel_size=5) # take out outliers
+    max_index = np.argmax(filtered_spectra)
+    cushion = 180
+    mask = np.ones(len(filtered_spectra), dtype=bool)
+    for i in range(len(mask)):
+        if i < max_index + cushion and i > max_index - cushion:
+            mask[i] = False
+    baseline = scipy.interpolate.interp1d(freq[mask], filtered_spectra[mask], kind='linear')
+    return baseline(freq)
+
+
+def get_gain(noise_on, noise_off):
+	"""
+	Calculates the gain which, when multiplied with our power spectra, converts our spectra 
+	to units of temperature brightness (Kelvin).
+
+	Parameters:
+	noise_on (numpy array): spectra with telescope noise turned on
+	noise_off (numpy array): specftra with telescope noise turned off
+
+	Returns:
+	gain (float): a multiplying constant
+	"""
+	noise_on_spectra = average_spectra(noise_on, len(noise_on))
+	noise_off_spectra = average_spectra(noise_off, len(noise_off))
+	T_noise = 30
+	T_sky = 2.73
+	gain =  (T_noise - T_sky) / np.sum(noise_on_spectra - noise_off_spectra) * np.sum(noise_off_spectra)
+	return gain
+
+noise_on = pyfits.open("Data/noise_on.fits")
+noise_off = pyfits.open("Data/noise_off.fits")
+gain = get_gain(noise_on, noise_off)
+
+
+def doppler_correction(observation):
+	"""
+	Determines the correction needed to shift the doppler velocity to the LSR frame
+
+	Parameters:
+	observation (file): file where spectra data is saved
+
+	Returns:
+	correction (float): a number that is subtracted from the doppler velocity
+	"""
+	obs_header = observation[0].header
+	ra, dec, jd = obs_header["RA"], obs_header["DEC"], obs_header["JD"]
+	return ugradio.doppler.get_projected_velocity(ra, dec, jd) / 1e3
+
+
+def doppler_velocity(observation, freq):
+	nu = 1420.4
+	delta_nu = freq - nu
+	c = 3e5
+	doppler_velocity = (delta_nu / nu) * c
+	return doppler_velocity - doppler_correction(observation)
+
+
+def calibrate_spectra(observation, n_spectra):
+	"""
+	Calibrates observation spectra such that we go from power spectra as a function
+	of frequency to temperature spectra as a function of doppler velocity (in the 
+	LSR frame)
+
+	Parameters:
+	observation (file): file where spectra data is saved
+
+	Returns:
+	n_spectra (int): the number of spectra taken during the observation
+	"""
+	average_spectra = average_spectra(observation, n_spectra)
+	freq = frequency(observation)
+	temperature_brightness = (average_spectra - baseline_fit(average_spectra, freq)) * gain
+	doppler_velocity = doppler_velocity(observation, freq)
+	return temperature_brightness, doppler_velocity
+
--- observation.py
+++ observation.py
@@ -4,9 +4,27 @@ import tool_box as tb
 import tracking
 import leuschner
 import ugradio.leusch
+from astropy.coordinates import SkyCoord,AltAz,EarthLocation
+from astropy import units as u
+from astropy.time import Time
+import ugradio.timing
 
 ALT_MIN, ALT_MAX = 15, 85
 AZ_MIN, AZ_MAX = 5, 350
+
+def print_altaz(alt, az):
+    print("Altitude: " + str(alt), "Azimuth: " + str(az))
+
+
+def get_altaz(l, b, julian_date=None):
+    if julian_date == None:
+        julian_date = ugradio.timing.julian_date()
+    position = SkyCoord(frame='galactic', l=l, b=b, unit=(u.degree,u.degree))
+    altaz=position.transform_to(AltAz(obstime=Time(julian_date,format='jd'),location=coords))
+    alt, az = altaz.alt.deg, altaz.az.deg
+    print_altaz(alt, az)
+    return alt, az
+
 
 def take_observation(filename):
     degree_spacing = 2
@@ -15,10 +33,11 @@ def take_observation(filename):
     number_of_spectra = 20
 
     synth = ugradio.agilent.SynthDirect()
+    synth.set_frequency(635, "MHz")
+
     LT = ugradio.leusch.LeuschTelescope()
     spectrometer = leuschner.Spectrometer('10.0.1.2')
-    missing_longitudes = np.load("missinglongitudes.npy")
-    synth.set_frequency(635, "MHz")
+    missing_longitudes = np.load("missing_longitudes.npy")
 
     for l in longitude_range:
         if (l in missing_longitudes):
@@ -28,30 +47,11 @@ def take_observation(filename):
                 spectrometer.read_spec(filename + str(l) + ".fits", number_of_spectra, (l, b))
     LT.stow()
 
-def get_spectra(observation, spectra_number, polarization="first"):
-    if polarization == "first":
-        pol = 'auto0_real'
-    elif polarization == "second":
-        pol = 'auto1_real'
-    spectra = observation[spectra_number].data[pol]
-    return spectra
-
-
-def plot_spectra(observation, n_spectra, title="Insert Title"):
-    power, freqs = average_spectra(observation, n_spectra)
-    self.power_plot = plt.figure(figsize = [15, 6])
-    plt.title(title)
-    plt.xlabel("Frequency (MHz)")
-    plt.ylabel("Power ($V^{2}$)")
-    plt.plot_power(power, freqs)
-    plt.grid()
-    plt.show()
-
-
-def average_spectra(observation, n_spectra):
-    average_spectra = []
-    for i in np.arange(1, n_spectra):
-        average_spectra.append(get_spectra(observation, i))
-    v_samp = observation[0].header["SAMPRATE"]
-    freqs = tb.shift(tb.freq(len(spectra), d = 1 / v_samp)) / 1e6
-    return np.mean(np.array(average_spectra), axis = 0), freqs
+def find_missing_longitudes():
+    missing_longitudes = []
+    for l in longitude_range:
+        try:
+            file = pyfits.open("Data/first_try" + str(l) + ".fits")
+        except:
+            missing_longitudes.append(l)
+    np.save("missing_longitudes", missing_longitudes)
